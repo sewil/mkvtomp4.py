45b0676ca229192191456ec2a3e581b24057f92f
Simon Haga <1498414+sewil@users.noreply.github.com>
2020-12-27 03:57:51 +0100

    Use subprocess instead

--- mkvtomp4.py
+++ mkvtomp4.py
@@ -2,19 +2,20 @@ import os
 import sys
 import shutil
 import json
+import subprocess
 
 # Functions
 def mkvextract(mkv_path, audio_track_index, video_path, audio_path):
   mkvextract_path = os.path.join(pwd, 'Tools', 'mkvextract', 'mkvextract')
   print(mkvextract_path)
-  mkvextract_call = mkvextract_path + ' tracks "' + mkv_path + '" 0:"' + video_path + '" ' + str(audio_track_index) + ':"' + audio_path + '"'
+  mkvextract_call = [mkvextract_path, 'tracks', mkv_path, '0:' + video_path, str(audio_track_index) + ':' + audio_path]
   print(mkvextract_call)
-  os.system(mkvextract_call)
+  subprocess.call(mkvextract_call)
 
 def get_mediainfo(mkv_path):
   mediainfo_path = os.path.join(pwd, 'Tools', 'MediaInfo', 'x64', 'MediaInfo')
   print(mediainfo_path)
-  mediainfo_command = mediainfo_path + ' --Full --Output=JSON "' + mkv_path + '"'
+  mediainfo_command = '"' + mediainfo_path + '" --Full --Output=JSON "' + mkv_path + '"'
   print(mediainfo_command)
   info = json.loads(os.popen(mediainfo_command).read())
   return info
@@ -22,30 +23,30 @@ def get_mediainfo(mkv_path):
 def delay_audio(delay, audio_path, audio_2_path):
   eac3to_path = os.path.join(pwd, "Tools", "eac3to", "eac3to")
   print(eac3to_path)
-  eac3to_call = eac3to_path + ' "' + audio_path + '" "' + audio_2_path + '" ' + delay + ' -progressnumbers'
+  eac3to_call = [eac3to_path, audio_path, audio_2_path, delay, '-progressnumbers']
   print(eac3to_call)
-  os.system(eac3to_call)
+  subprocess.call(eac3to_call)
 
 def convert_audio(audio_out_path, audio_2_path):
   azid_path = os.path.join(pwd, 'Tools', 'Azid', 'azid')
   print(azid_path)
   wav_path = os.path.join(pwd, 'tmp', 'audio.wav')
   print(wav_path)
-  azid_command = azid_path + ' -d 3/2 -l 1.0 -L 0.0 -o L,R,C,LFE,SL,SR -a -n true "' + audio_2_path + '" "' + wav_path + '"'
+  azid_command = [azid_path, '-d', '3/2', '-l', '1.0', '-L', '0.0', '-o', 'L,R,C,LFE,SL,SR', '-a', '-n', 'true', audio_2_path, wav_path]
   print(azid_command)
-  os.system(azid_command)
+  subprocess.call(azid_command)
 
   qaac_path = os.path.join(pwd, 'Tools', 'qaac', 'qaac')
   print(qaac_path)
-  qaac_command = qaac_path + ' --quality 2 --ignorelength --tvbr 95 "' + wav_path + '" -o "' + audio_out_path + '"'
+  qaac_command = [qaac_path, '--quality', '2', '--ignorelength', '--tvbr', '95', wav_path, '-o', audio_out_path]
   print(qaac_command)
-  os.system(qaac_command)
+  subprocess.call(qaac_command)
 
 def mp4box(video_path, audio_out_path, framerate, out_path):
   mp4box_path = os.path.join(pwd, 'Tools', 'mp4box', 'x64', 'MP4Box.exe')
-  mp4box_command = mp4box_path + ' -add "' + video_path + '":fps=' + framerate + ' -add "' + audio_out_path + '":fps=' + framerate + ' "' + out_path + '"'
+  mp4box_command = [mp4box_path, '-add', video_path + ':fps=' + framerate, '-add', audio_out_path + ':fps=' + framerate, out_path]
   print(mp4box_command)
-  os.system(mp4box_command)
+  subprocess.call(mp4box_command)
   return
 
 if (len(sys.argv) < 3):
@@ -56,17 +57,20 @@ if (len(sys.argv) < 3):
 pwd = os.path.dirname(os.path.realpath(__file__))
 print(pwd)
 
-audio_track_index = int(sys.argv[2])
-
 out_path = sys.argv[3]
 print(out_path)
 
 mkv_path = sys.argv[1]
+print(mkv_path)
 info = get_mediainfo(mkv_path)
+print(info)
 
 video_track = info['media']['track'][0]
 framerate = video_track['FrameRate']
 print(framerate)
+
+audio_track_index = int(sys.argv[2])
+print(audio_track_index)
 
 audio_track = info['media']['track'][audio_track_index + 1]
 audio_format = audio_track['Format']
